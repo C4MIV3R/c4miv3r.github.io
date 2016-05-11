be43f867655d996f80b0217049aa213692666c69
cam iverson <camiver@gmail.com>
2016-05-11 12:02:06 -0500

    removed Angular idea - not worth it

--- public/scripts/portfolio.js
+++
@@ -1,8 +0,0 @@
-// create the module and name it
-var portfolio = angular.module('portfolio',[]);
-
-// create the controller and inject Angular's $scope
-portfolio.controller('mainController', function($scope){
-    // create a message to display in our view
-    $scope.message = 'Check me out.';
-})
