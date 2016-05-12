2504e3c37324806fa01253c015e914ea7b87a338
cam iverson <camiver@gmail.com>
2016-05-12 13:25:08 -0500

    cleaned up script

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,9 +1,8 @@
 // on mouseover and mouseout - show or hide element
 $('.contact-item').on('mouseover', function() {
-  console.log('OW FUCK STOP POKING ME!');
+  // console.log('OW F!@#, STOP POKING ME!');
   // how to select only the hovered element? this?
   var wantedDiv = $(this).siblings('#contactInfo');
-  console.log(wantedDiv);
   $(wantedDiv).animate({
     opacity: 1,
     width: '130px',
@@ -12,7 +11,7 @@ $('.contact-item').on('mouseover', function() {
 });
 $('.contact-item').on('mouseout', function() {
   var wantedDiv = $(this).siblings('#contactInfo');
-  console.log('Thank you, asshole.');
+  // console.log('Thank you, @$$#!)^&.');
   $(wantedDiv).animate({
     opacity: 0,
     width: '0px',
